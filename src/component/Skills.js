488a0b188f2ff80054b7c5e84e8f638707f28af1
Code With Mercy <64808015+mercyharbo@users.noreply.github.com>
2021-12-10 15:19:08 +0100

    Add files via upload

---
+++ src/component/Skills.js
@@ -0,0 +1,22 @@
+const Skills = () => {
+    return (
+        <div className="skills_wrapper">
+            <h1> Skills </h1>
+            <div className="skills">
+                <ul>
+                    <li> JavaScript ( ES6 ) </li>
+                    <li> React js </li>
+                    <li> Node (Express) js </li>
+                    <li> Sass/SCSS </li>
+                    <li> CSS 3 </li>
+                    <li> Git & GitHub </li>
+                    <li> HTML 5 </li>
+                    <li> Bootstrap </li>
+                    <li> WordPress </li>
+                </ul>
+            </div>
+        </div>
+     );
+}
+ 
+export default Skills;
---
+++ src/component/contact.js
@@ -0,0 +1,15 @@
+const GetInTouch = () => {
+    return (
+        <div className="contact">
+            <h1> Get In Touch </h1>
+            <p>
+                I’m currently looking for job opportunities with these skills, my inbox is always open.
+                Whether you have a question or just want to say hi, let's collaborate on project.
+            </p>
+
+            <button> <a href="mailto:damilare791@gmail.com"> Say Hi </a> </button>
+        </div>
+     );
+}
+ 
+export default GetInTouch;
