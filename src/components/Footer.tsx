8ec85aa42eab0b2fa541882bb881af82ad41930e
Afolabi Ridwan Damilare <damilare791@gmail.com>
2023-10-10 10:29:18 +0100

    comepleted full page

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -13,33 +13,19 @@ const navLinks = [
 export default function Footer() {
   return (
     <footer className='flex flex-col justify-center items-center gap-10 py-[5rem]  mx-auto xl:px-0 xl:w-[90%] md:w-full md:px-10 sm:px-5 sm:w-full '>
-      <div className='flex justify-between items-center w-full xl:flex-row xl:gap-0 md:flex-col md:gap-10 sm:flex-col sm:gap-10  '>
-        <div className='xl:w-[50%] md:w-full sm:w-full '>
-          <div className='flex capitalize xl:justify-start xl:items-center xl:gap-10 md:justify-center md:items-center md:gap-5 sm:justify-center sm:items-center sm:gap-5 '>
-            {navLinks.slice(0, 4).map((link) => {
-              return (
-                <Link href={link.href} key={link.id} className=''>
-                  {link.name}
-                </Link>
-              )
-            })}
-          </div>
-        </div>
-
-        <div className='flex justify-end items-center gap-10 text-xl'>
-          <Link href='https://github.com/mercyharbo'>
-            <BsGithub />
-          </Link>
-          <Link href='https://twitter.com/codewithmercy'>
-            <BsTwitter />
-          </Link>
-          <Link href='https://www.linkedin.com/in/codewithmercy'>
-            <BsLinkedin />
-          </Link>
-          <Link href='https://www.instagram.com/codewithmercy/'>
-            <BsInstagram />
-          </Link>
-        </div>
+      <div className='flex xl:justify-start md:justify-center sm:justify-center items-center gap-10 text-xl w-full'>
+        <Link href='https://github.com/mercyharbo'>
+          <BsGithub />
+        </Link>
+        <Link href='https://twitter.com/codewithmercy'>
+          <BsTwitter />
+        </Link>
+        <Link href='https://www.linkedin.com/in/codewithmercy'>
+          <BsLinkedin />
+        </Link>
+        <Link href='https://www.instagram.com/codewithmercy/'>
+          <BsInstagram />
+        </Link>
       </div>
       <hr className='w-full ' />
       <p className='text-center'>
